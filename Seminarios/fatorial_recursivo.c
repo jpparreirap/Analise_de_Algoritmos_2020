64626f25df2840e47f9b53ba959f7c22a9b233f7
João Pedro Oliveira Silva <joaopedro2195@gmail.com>
2020-10-29 09:51:34 -0400

    Add files via upload

---
+++ Seminarios/fatorial_recursivo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fat(n) {
+    if ((n == 1) || (n == 0)) return 1;
+    else return fat(n-1)*n;
+}
+
+main() {
+    int n;
+    printf("Informe o numero que deseja obter o fatorial:\n");
+    scanf("%d", &n);
+    printf("O fatorial de %d = %d", n, fat(n));
+}
